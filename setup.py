717990875027eb09bab9440b6e69c57cb51699fe
Carlos Hernandez-Vaquero <carlos.hernandez.vaquero@gmail.com>
2020-07-12 13:41:47 +0200

    Update setup.py with author name

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ setup(name='distributions',
       version='0.1',
       description='Gaussian distributions',
       packages=['distributions'],
+      author='Carlos Hernandez-Vaquero',
       zip_safe=False)
